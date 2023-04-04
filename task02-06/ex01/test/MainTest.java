7163c7386db1a26f256732aed83b72b8b4d2d5f7
Олександр Куценко <120801771+linbaz@users.noreply.github.com>
2023-04-04 12:16:25 +0300

    Add files via upload

---
+++ task02-06/ex01/test/MainTest.java
@@ -0,0 +1,46 @@
+package ex01;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testInit() {
+        Сount calc = new Сount();
+        calc.init(4);
+        Visual result = calc.getResult();
+        assertEquals(4, result.getDecimalNumber());
+        assertEquals(0, result.getTetradsCount());
+    }
+
+    @Test
+    public void testSaveAndRestore() throws Exception {
+        Сount calc = new Сount();
+        calc.init(4);
+        calc.save();
+
+        Сount calc2 = new Сount();
+        calc2.restore();
+
+        assertEquals(calc.getResult().getDecimalNumber(), calc2.getResult().getDecimalNumber());
+        assertEquals(calc.getResult().getTetradsCount(), calc2.getResult().getTetradsCount());
+    }
+    @Test
+    public void testSetValuesAndGetters() {
+        Visual item = new Visual();
+        item.setValues(4, 4);
+
+        assertEquals(4, item.getDecimalNumber());
+        assertEquals(4, item.getTetradsCount());
+    }
+
+    @Test
+    public void testToString() {
+        Visual item = new Visual(4, 3);
+
+        String expectedString = "Decimal number = 4, Tetrads count = 3";
+        assertEquals(expectedString, item.toString());
+    }
+
+}
